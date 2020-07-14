0e192099062bb1aab71efbb210625a560f292981
Carlos Coral <coralcarlos21@gmail.com>
2020-07-13 23:53:23 -0500

    Updates

---
+++ precache-manifest.9c338bc274021f8e3926d80213002187.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e00a0d83483323acb54d54194bef183",
+    "url": "/anonymous-chat/index.html"
+  },
+  {
+    "revision": "4f8c3a1057fbf2cfe7c7",
+    "url": "/anonymous-chat/static/css/main.0fccd787.chunk.css"
+  },
+  {
+    "revision": "e8addef24e3ad18268d0",
+    "url": "/anonymous-chat/static/js/2.2229bda0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/anonymous-chat/static/js/2.2229bda0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f8c3a1057fbf2cfe7c7",
+    "url": "/anonymous-chat/static/js/main.6be37763.chunk.js"
+  },
+  {
+    "revision": "7d6b03c7267ffb813e3e",
+    "url": "/anonymous-chat/static/js/runtime-main.b203786e.js"
+  }
+]);
